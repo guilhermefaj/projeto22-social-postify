79013c3c0ec74e79b38c2769af60d0b97fa22d63
guilhermefaj <figueiredoguiu@gmail.com>
2023-08-22 13:51:09 -0300

    Feat: create and get medias in memory

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MediasModule } from './medias/medias/medias.module';
-import { MediasModule } from './medias/medias.module';
 import { PublicationsModule } from './publications/publications.module';
 import { PostsModule } from './posts/posts.module';
 import { MediasModule } from './medias/medias.module';
-
 @Module({
   imports: [MediasModule, PostsModule, PublicationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/medias/medias.service.ts
+++ src/medias/medias.service.ts
@@ -1,15 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateMediaDto } from './dto/create-media.dto';
 import { UpdateMediaDto } from './dto/update-media.dto';
+import { Media } from './entities/media.entity';
 
 @Injectable()
 export class MediasService {
+
+  private medias: Media[];
+
+  constructor() {
+    this.medias = []
+  }
+
   create(createMediaDto: CreateMediaDto) {
-    return 'This action adds a new media';
+    const media = new Media(createMediaDto.title, createMediaDto.username);
+
+    if (!createMediaDto.title || !createMediaDto.username) {
+      throw new HttpException('Campos obrigatórios ausentes', HttpStatus.BAD_REQUEST)
+    }
+
+    //TODO verificar se já existe title e username iguais antes de criar
+
+    return this.medias.push(media);
   }
 
   findAll() {
-    return `This action returns all medias`;
+    return this.medias;
   }
 
   findOne(id: number) {
